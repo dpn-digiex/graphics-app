ffc4ae92dff0402801203c084ab085168ff7ee49
Phong Nguyen <phchmessibar326@gmail.com>
2025-02-24 09:13:40 +0700

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace GraphicsApp
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSideBar = new System.Windows.Forms.Panel();
+            this.btnArc = new System.Windows.Forms.Button();
+            this.btnRect = new System.Windows.Forms.Button();
+            this.btnEclipse = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelSelection = new System.Windows.Forms.Label();
+            this.btnRemoveSelection = new System.Windows.Forms.Button();
+            this.btnRemoveShapes = new System.Windows.Forms.Button();
+            this.dropdownColor = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputWidthPen = new System.Windows.Forms.TextBox();
+            this.panelSideBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSideBar
+            // 
+            this.panelSideBar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelSideBar.Controls.Add(this.btnArc);
+            this.panelSideBar.Controls.Add(this.btnRect);
+            this.panelSideBar.Controls.Add(this.btnEclipse);
+            this.panelSideBar.Controls.Add(this.btnLine);
+            this.panelSideBar.Location = new System.Drawing.Point(1, 0);
+            this.panelSideBar.Name = "panelSideBar";
+            this.panelSideBar.Size = new System.Drawing.Size(200, 508);
+            this.panelSideBar.TabIndex = 0;
+            // 
+            // btnArc
+            // 
+            this.btnArc.Location = new System.Drawing.Point(11, 126);
+            this.btnArc.Name = "btnArc";
+            this.btnArc.Size = new System.Drawing.Size(165, 32);
+            this.btnArc.TabIndex = 5;
+            this.btnArc.Text = "Hình vòng cung";
+            this.btnArc.UseVisualStyleBackColor = true;
+            this.btnArc.Click += new System.EventHandler(this.btnArc_Click);
+            // 
+            // btnRect
+            // 
+            this.btnRect.Location = new System.Drawing.Point(11, 88);
+            this.btnRect.Name = "btnRect";
+            this.btnRect.Size = new System.Drawing.Size(165, 32);
+            this.btnRect.TabIndex = 4;
+            this.btnRect.Text = "Hình chữ nhật";
+            this.btnRect.UseVisualStyleBackColor = true;
+            this.btnRect.Click += new System.EventHandler(this.btnRect_Click);
+            // 
+            // btnEclipse
+            // 
+            this.btnEclipse.Location = new System.Drawing.Point(11, 50);
+            this.btnEclipse.Name = "btnEclipse";
+            this.btnEclipse.Size = new System.Drawing.Size(165, 32);
+            this.btnEclipse.TabIndex = 3;
+            this.btnEclipse.Text = "Hình tròn";
+            this.btnEclipse.UseVisualStyleBackColor = true;
+            this.btnEclipse.Click += new System.EventHandler(this.btnEclipse_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(11, 12);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(165, 32);
+            this.btnLine.TabIndex = 2;
+            this.btnLine.Text = "Đường thẳng";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.BackColor = System.Drawing.SystemColors.Info;
+            this.panelCanvas.Location = new System.Drawing.Point(207, 34);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(800, 474);
+            this.panelCanvas.TabIndex = 1;
+            this.panelCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCanvas_Paint);
+            this.panelCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseDown);
+            this.panelCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseMove);
+            this.panelCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bạn đang chọn vẽ:";
+            this.label1.Click += new System.EventHandler(this.formMain_Load);
+            // 
+            // labelSelection
+            // 
+            this.labelSelection.AutoSize = true;
+            this.labelSelection.Location = new System.Drawing.Point(340, 9);
+            this.labelSelection.Name = "labelSelection";
+            this.labelSelection.Size = new System.Drawing.Size(0, 16);
+            this.labelSelection.TabIndex = 2;
+            // 
+            // btnRemoveSelection
+            // 
+            this.btnRemoveSelection.Location = new System.Drawing.Point(448, 5);
+            this.btnRemoveSelection.Name = "btnRemoveSelection";
+            this.btnRemoveSelection.Size = new System.Drawing.Size(75, 24);
+            this.btnRemoveSelection.TabIndex = 3;
+            this.btnRemoveSelection.Text = "Xóa";
+            this.btnRemoveSelection.UseVisualStyleBackColor = true;
+            this.btnRemoveSelection.Click += new System.EventHandler(this.btnRemoveSelection_Click);
+            // 
+            // btnRemoveShapes
+            // 
+            this.btnRemoveShapes.Location = new System.Drawing.Point(529, 5);
+            this.btnRemoveShapes.Name = "btnRemoveShapes";
+            this.btnRemoveShapes.Size = new System.Drawing.Size(146, 24);
+            this.btnRemoveShapes.TabIndex = 4;
+            this.btnRemoveShapes.Text = "Xóa tất cả hình vẽ";
+            this.btnRemoveShapes.UseVisualStyleBackColor = true;
+            this.btnRemoveShapes.Click += new System.EventHandler(this.btnRemoveShapes_Click);
+            // 
+            // dropdownColor
+            // 
+            this.dropdownColor.FormattingEnabled = true;
+            this.dropdownColor.Location = new System.Drawing.Point(679, 5);
+            this.dropdownColor.Name = "dropdownColor";
+            this.dropdownColor.Size = new System.Drawing.Size(121, 24);
+            this.dropdownColor.TabIndex = 5;
+            this.dropdownColor.Text = "Chọn màu sắc";
+            this.dropdownColor.SelectedIndexChanged += new System.EventHandler(this.dropdownColor_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label2.Location = new System.Drawing.Point(806, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kích thước bút vẽ:";
+            // 
+            // inputWidthPen
+            // 
+            this.inputWidthPen.Location = new System.Drawing.Point(933, 5);
+            this.inputWidthPen.Name = "inputWidthPen";
+            this.inputWidthPen.Size = new System.Drawing.Size(48, 22);
+            this.inputWidthPen.TabIndex = 7;
+            this.inputWidthPen.Text = "5";
+            this.inputWidthPen.TextChanged += new System.EventHandler(this.inputWidthPen_TextChanged);
+            this.inputWidthPen.Leave += new System.EventHandler(this.inputWidthPen_Leave);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 509);
+            this.Controls.Add(this.inputWidthPen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dropdownColor);
+            this.Controls.Add(this.btnRemoveShapes);
+            this.Controls.Add(this.btnRemoveSelection);
+            this.Controls.Add(this.labelSelection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelSideBar);
+            this.Controls.Add(this.panelCanvas);
+            this.Name = "formMain";
+            this.Text = "Graphics Application";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.panelSideBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSideBar;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Panel panelCanvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelSelection;
+        private System.Windows.Forms.Button btnRemoveSelection;
+        private System.Windows.Forms.Button btnEclipse;
+        private System.Windows.Forms.Button btnRect;
+        private System.Windows.Forms.Button btnRemoveShapes;
+        private System.Windows.Forms.Button btnArc;
+        private System.Windows.Forms.ComboBox dropdownColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputWidthPen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GraphicsApp
+{
+    public partial class formMain : Form
+    {
+        Graphics gp;
+        Pen myPen;
+
+        Color selectedColor;
+        int widthPen;
+        private Dictionary<string, Color> colorMap;
+
+        bool isPress = true; // a flag
+
+        bool isSelectedLine = false;
+        bool isSelectedRect = false;
+        bool isSelectedEcllipse = false;
+        bool isSelectedArc = false;
+        bool isSelectedShape
+        {
+            get
+            {
+                return this.isSelectedLine
+                    || this.isSelectedRect
+                    || this.isSelectedEcllipse
+                    || this.isSelectedArc;
+            }
+        }
+
+        List<DrawShape> lstObject = new List<DrawShape>();
+
+        DrawShape selectedShape = null;
+        List<DrawShape> selectedShapesGroup = new List<DrawShape>();
+
+        public formMain()
+        {
+            InitializeComponent();
+            InitializeUI();
+            gp = this.panelCanvas.CreateGraphics();
+            selectedColor = Color.Blue;
+            widthPen = 5;
+            myPen = new Pen(selectedColor, widthPen);
+        }
+
+        private void InitializeUI()
+        {
+            dropdownColor.DropDownStyle = ComboBoxStyle.DropDownList;
+            colorMap = new Dictionary<string, Color>
+            {
+                { "Xanh Dương", Color.Blue },
+                { "Đỏ", Color.Red },
+                { "Xanh Lá", Color.Green },
+                { "Vàng", Color.Yellow },
+                { "Đen", Color.Black },
+                { "Trắng", Color.White },
+                { "Xám", Color.Gray },
+                { "Tím", Color.Purple },
+                { "Hồng", Color.Pink },
+                { "Cam", Color.Orange },
+            };
+
+            foreach (var color in colorMap.Keys)
+            {
+                dropdownColor.Items.Add(color);
+            }
+            dropdownColor.SelectedIndex = 0;
+        }
+
+        private void formMain_Load(object sender, EventArgs e) { }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            this.isSelectedLine = true;
+            this.isSelectedRect = false;
+            this.isSelectedEcllipse = false;
+            this.isSelectedArc = false;
+            labelSelection.Text = btnLine.Text;
+        }
+
+        private void btnEclipse_Click(object sender, EventArgs e)
+        {
+            this.isSelectedEcllipse = true;
+            this.isSelectedLine = false;
+            this.isSelectedRect = false;
+            this.isSelectedArc = false;
+            labelSelection.Text = btnEclipse.Text;
+        }
+
+        private void btnRect_Click(object sender, EventArgs e)
+        {
+            this.isSelectedRect = true;
+            this.isSelectedLine = false;
+            this.isSelectedEcllipse = false;
+            this.isSelectedArc = false;
+            labelSelection.Text = btnRect.Text;
+        }
+
+        private void btnArc_Click(object sender, EventArgs e)
+        {
+            this.isSelectedArc = true;
+            this.isSelectedLine = false;
+            this.isSelectedEcllipse = false;
+            this.isSelectedRect = false;
+            labelSelection.Text = btnArc.Text;
+        }
+
+        private void btnRemoveShapes_Click(object sender, EventArgs e)
+        {
+            this.lstObject.Clear();
+            panelCanvas.Refresh();
+        }
+
+        private void panelCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var shape in lstObject)
+            {
+                using (Pen pen = new Pen(shape.color, shape.width))
+                {
+                    shape.Draw(e.Graphics, pen);
+                }
+            }
+        }
+
+        private void panelCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.isPress == true && this.lstObject.Count > 0)
+            {
+                this.lstObject[this.lstObject.Count - 1].p2 = e.Location;
+                this.panelCanvas.Refresh();
+            }
+        }
+
+        private void panelCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.isPress = true;
+            if (!isSelectedShape)
+            {
+                MessageBox.Show(
+                    "Vui lòng chọn hình vẽ để bắt đầu vẽ!",
+                    "Thông báo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                return;
+            }
+            if (isSelectedShape)
+            {
+                DrawShape myObj = null;
+
+                if (isSelectedLine)
+                    myObj = new LineShape();
+                if (isSelectedEcllipse)
+                    myObj = new EllipseShape();
+                if (isSelectedRect)
+                    myObj = new RectShape();
+                if (isSelectedArc)
+                    myObj = new ArcShape();
+                if (myObj != null)
+                {
+                    myObj.p1 = e.Location;
+                    myObj.color = selectedColor;
+                    myObj.width = widthPen;
+                    lstObject.Add(myObj);
+                }
+            }
+        }
+
+        private void panelCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.isPress = false;
+            this.lstObject[this.lstObject.Count - 1].p2 = e.Location;
+            this.panelCanvas.Refresh();
+            // this.isSelectedRect = false;
+            // this.isSelectedEcllipse = false;
+            // this.isSelectedLine = false;
+        }
+
+        private void btnRemoveSelection_Click(object sender, EventArgs e)
+        {
+            labelSelection.Text = "";
+            isPress = false;
+            isSelectedLine = false;
+            isSelectedEcllipse = false;
+            isSelectedRect = false;
+        }
+
+        public abstract class DrawShape
+        {
+            public Point p1;
+            public Point p2;
+            public int width;
+            public Color color;
+
+            public Rectangle boundingRect
+            {
+                get
+                {
+                    int x = Math.Min(p1.X, p2.X);
+                    int y = Math.Min(p1.Y, p2.Y);
+                    int width = Math.Abs(p2.X - p1.X);
+                    int height = Math.Abs(p2.Y - p1.Y);
+                    return new Rectangle(x, y, width, height);
+                }
+            }
+            public abstract void Draw(Graphics myGp, Pen myPen);
+        };
+
+        public class LineShape : DrawShape
+        {
+            public override void Draw(Graphics myGp, Pen myPen)
+            {
+                myGp.DrawLine(myPen, this.p1, this.p2);
+            }
+        };
+
+        public class EllipseShape : DrawShape
+        {
+            public override void Draw(Graphics myGp, Pen myPen)
+            {
+                myGp.DrawEllipse(
+                    myPen,
+                    this.p1.X,
+                    this.p1.Y,
+                    this.p2.X - this.p1.X,
+                    this.p2.Y - this.p1.Y
+                );
+            }
+        };
+
+        public class RectShape : DrawShape
+        {
+            public override void Draw(Graphics myGp, Pen myPen)
+            {
+                myGp.DrawRectangle(myPen, this.p1.X, this.p1.Y, this.p2.X, this.p2.Y);
+            }
+        };
+
+        public class ArcShape : DrawShape
+        {
+            public override void Draw(Graphics myGp, Pen myPen)
+            {
+                int x = Math.Min(this.p1.X, this.p2.X);
+                int y = Math.Min(this.p1.Y, this.p2.Y);
+                int width = Math.Abs(this.p2.X - this.p1.X);
+                int height = Math.Abs(this.p2.Y - this.p1.Y);
+
+                float startAngle = 0;
+                float sweepAngle = 180;
+                if (width == 0 || height == 0)
+                {
+                    Console.WriteLine("invalid width, height arc.");
+                    return;
+                }
+
+                myGp.DrawArc(myPen, x, y, width, height, startAngle, sweepAngle);
+            }
+        };
+
+        private void dropdownColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedColor = dropdownColor.SelectedItem.ToString();
+            this.selectedColor = colorMap[selectedColor];
+        }
+
+        private void inputWidthPen_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputWidthPen.Text))
+            {
+                return;
+            }
+
+            if (int.TryParse(inputWidthPen.Text, out int value) && value > 0)
+            {
+                this.widthPen = value;
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Giá trị nhập sai! Vui lòng nhập số nguyên dương.",
+                    "Thông báo lỗi",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+
+        private void inputWidthPen_Leave(object sender, EventArgs e)
+        {
+            if (
+                string.IsNullOrWhiteSpace(inputWidthPen.Text)
+                || !int.TryParse(inputWidthPen.Text, out int value)
+                || value <= 0
+            )
+            {
+                inputWidthPen.Text = this.widthPen.ToString();
+            }
+        }
+    }
+}
